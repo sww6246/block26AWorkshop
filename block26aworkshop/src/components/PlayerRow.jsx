1493fc213acfb018f745473d5af53409defaac48
sww6246 <sam.weber7@gmail.com>
2025-04-08 12:53:55 -0600

    Final HW Commit

--- block26aworkshop/src/components/PlayerRow.jsx
+++ block26aworkshop/src/components/PlayerRow.jsx
@@ -4,8 +4,8 @@ export default function PlayerRow({ player, setSelectedPlayerId}){
     return (
         <div className="card">
             <h2>{player.first_name} {player.last_name}</h2>
-            <p>Position: {player.position}</p>
-            <p>College: {player.college}</p>
+            <p><strong>Position:</strong> {player.position}</p>
+            <p><strong>College:</strong> {player.college}</p>
             <button onClick={()=>setSelectedPlayerId(player.id)}>Learn More</button>
         </div>
         
